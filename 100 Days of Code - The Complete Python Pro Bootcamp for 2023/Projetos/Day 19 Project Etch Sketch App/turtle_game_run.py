951a163c74389fe4d12a464e6b29dab8a6cb927d
jvroot <joaovlima.delima@gmail.com>
2024-01-24 17:31:25 -0300

    turtle run game and gitignore

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/Projetos/Day 19 Project Etch Sketch App/turtle_game_run.py
@@ -0,0 +1,49 @@
+from turtle import Turtle, Screen
+import random
+import random
+
+
+tim = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+
+user_bet = screen.textinput(title = 'Make your bet', prompt= 'Which turtle will win the race? Enter a color: ')
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+all_turtles = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=-100 + turtle_index * 30)
+    all_turtles.append(new_turtle)
+
+
+
+if user_bet:
+    is_racer_on = True
+    
+
+while is_racer_on:
+    
+    for turtle in all_turtles:
+        
+        
+        if turtle.xcor() > 230:
+            is_racer_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f'You won! The {winning_color} turtle is the winner!')
+            else:
+                print(f'You lost! The {winning_color} turtle is the winner!')
+        
+        
+        
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+    
+    
+
+screen.exitonclick()
