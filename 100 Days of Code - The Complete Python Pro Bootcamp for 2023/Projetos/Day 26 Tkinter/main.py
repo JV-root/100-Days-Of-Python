3fa1b3b0521adb9de20b80e46bd1da61339f39d8
jvroot <joaovlima.delima@gmail.com>
2024-02-03 13:13:15 -0300

    Refactor Tkinter GUI code and add playground.py

--- 100 Days of Code - The Complete Python Pro Bootcamp for 2023/Projetos/Day 26 Tkinter/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2023/Projetos/Day 26 Tkinter/main.py
@@ -1,32 +1,41 @@
 from tkinter import *
 
 
-window = tk.Tk()
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
-
-
-# Label
-my_label = tk.Label(text="I am a Label", font=("Arial", 24, "bold"))
-
-# Pack
-
-my_label['text'] = "New Text"  
+#Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
-
-my_label.pack()
-
-
-# Entry
-
-input = tk.Entry(width=10)
-
-input.pack()
-
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 #Button
-buttojn = tk.Button(text="Click Me")
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+#Entry
+input = Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
+
+
+
+
+
+
+
 
 
 window.mainloop()
