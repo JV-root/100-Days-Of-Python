2669f4bfda96450f8277d80e3e6d49981578bb30
jvroot <joaovlima.delima@gmail.com>
2024-02-16 23:12:44 -0300

    Exercicios Semana 03 Algo - Univesp

---
+++ Univesp/Algoritmos e Programação de Computadores I/Semana 3/Exercicio_01.py
@@ -0,0 +1,16 @@
+'''
+Primeiro, execute a atribuição
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+Agora, escreva duas expressões Python que são avaliadas, respectivamente,
+como a primeiro e a última palavras em palavras, na ordem do dicionário.
+
+'''
+
+
+palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+
+print(palavras[0])
+
+print(palavras[-1])
+
+# taco
